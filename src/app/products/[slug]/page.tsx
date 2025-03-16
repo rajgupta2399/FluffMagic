5c828b5d3158360b4634f0ad660a13be2c56f3fb
rajgupta2399 <rajkumardotnet2499@gmail.com>
2025-03-16 08:36:18 +0530

    update

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,41 +1,38 @@
 import { getProductBySlug } from "@/wix-api/products";
 import { notFound } from "next/navigation";
 import ProductDetails from "./ProductDetails";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 
 interface PageProps {
   params: { slug: string };
 }
 
-// export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+// export async function generateMetadata({
+//   params: { slug },
+// }: PageProps): Promise<Metadata> {
+//   const product = await getProductBySlug(slug);
 
-export async function generateMetadata({
-  params: { slug },
-}: PageProps): Promise<Metadata> {
-  // const { slug } = params;
-  const product = await getProductBySlug(slug);
+//   if (!product) notFound();
 
-  if (!product) notFound();
+//   const mainImage = product.media?.mainMedia?.image;
 
-  const mainImage = product.media?.mainMedia?.image;
-
-  return {
-    title: product.name,
-    description: "Get this product on Fluff Magic",
-    openGraph: {
-      images: mainImage?.url
-        ? [
-            {
-              url: mainImage.url,
-              width: mainImage.width,
-              height: mainImage.height,
-              alt: mainImage.altText || "",
-            },
-          ]
-        : undefined,
-    },
-  };
-}
+//   return {
+//     title: product.name,
+//     description: "Get this product on Fluff Magic",
+//     openGraph: {
+//       images: mainImage?.url
+//         ? [
+//             {
+//               url: mainImage.url,
+//               width: mainImage.width,
+//               height: mainImage.height,
+//               alt: mainImage.altText || "",
+//             },
+//           ]
+//         : undefined,
+//     },
+//   };
+// }
 
 export default async function Page({ params }: PageProps) {
   const { slug } = params;
