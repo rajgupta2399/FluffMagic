31e92abeea24b783e00826976a69acfe78862410
rajgupta2399 <rajkumardotnet2499@gmail.com>
2025-03-30 08:54:04 +0530

    desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Fluff Magic",
-  description: "The Fluff Magic E-Commerce Shop For Soft Toys",
+  description: "The Fluff Magic E-Commerce Shop For Soft Toys & Baby Products",
 };
 
 export default function RootLayout({
